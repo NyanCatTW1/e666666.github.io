b740057666d89e7d55efd15cc9caafd926c5e6a9
pg132 <42481851+pg132@users.noreply.github.com>
2018-09-06 07:10:24 -0700

    Syntax error...

--- javascripts/core/galactic_sacrifice.js
+++ javascripts/core/galactic_sacrifice.js
@@ -117,7 +117,7 @@ let galUpgrade32 = function () {
     x = x.min(Number.MAX_VALUE);
   }
   if (player.currentEternityChall==="") return x.pow(0.003).add(1);
-  return x.pow(0.0001).plus(1)
+  return x.pow(0.0001).add(1)
 }
 
 let galUpgrade33 = function () {
