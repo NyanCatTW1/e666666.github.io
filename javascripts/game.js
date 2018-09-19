1f61006d015293d1c20b0437729054ea36cb7e26
pg132 <42481851+pg132@users.noreply.github.com>
2018-09-18 17:00:41 -0700

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -5736,7 +5736,7 @@ function gameLoop(diff) {
     document.getElementById("ec1reward").textContent = "Reward: "+shortenMoney(Math.pow(Math.max(player.thisEternity*10, 1), 1.2+(ECTimesCompleted("eterc1")*0.2)))+"x post-dilation on all Time Dimensions (based on time spent this Eternity)"
     document.getElementById("ec2reward").textContent = "Reward: Infinity power affects 1st Infinity Dimension with reduced effect, Currently: "+shortenMoney(new Decimal(player.infinityPower.plus(10).log10()).pow(1000).times(player.infinityPower.pow(4.5/(700 - ECTimesCompleted("eterc2")*100))).max(1))+"x"
     document.getElementById("ec3reward").textContent = "Reward: Increase the multiplier for buying 10 dimensions, Currently: "+getDimensionPowerMultiplier().toFixed(2)+"x"
-    document.getElementById("ec4reward").textContent = "Reward: Infinity Dimension multiplier from unspent IP, Currently: "+shortenMoney(player.infinityPoints.pow(0.09 + ECTimesCompleted("eterc4")*0.06))+"x"
+    document.getElementById("ec4reward").textContent = "Reward: Infinity Dimension multiplier from unspent IP, Currently: "+shortenMoney(player.infinityPoints.pow(0.4 + ECTimesCompleted("eterc4")*0.2))+"x"
     document.getElementById("ec5reward").textContent = "Reward: Galaxy cost scaling starts "+((ECTimesCompleted("eterc5")*5))+" galaxies later."
     document.getElementById("ec6reward").textContent = "Reward: Further reduce the dimension cost multiplier increase, Currently: "+player.dimensionMultDecrease.toFixed(1)+"x "
     document.getElementById("ec7reward").textContent = "Reward: First Time dimension produces Eighth Infinity Dimensions, Currently: "+shortenMoney(getTimeDimensionProduction(1).pow(ECTimesCompleted("eterc7")*0.2).minus(1).max(0))+" per second. "
